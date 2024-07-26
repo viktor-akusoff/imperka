548ba8b5e8f42719ce2794313970683d875db19b
viktor-akusoff <kirill.voloshin.98@ya.ru>
2024-07-26 12:23:10 +0300

    deleting pages old content

--- frontend/pages/[page].vue
+++ frontend/pages/[page].vue
@@ -1,13 +1,5 @@
 <template>
-    <div class="w-50 m-5">
-        <PageHeader titleText="IT-студия «ИМПЕРКА»" :mediaSource="imperka">
-            НSжен сайт, который продает? Бот, который автоматизирует бизнес?
-            IT-студия "Имперка" к вашим услугам! Мы предлагаем фуллстэк-разработку любой сложности.
-            С нами вы получите работающий продукт в кратчайшие сроки.
-        </PageHeader>
-    </div>
 </template>
 
 <script setup lang="ts">
-    import imperka from 'assets/webm/imperka.webm'
 </script>
