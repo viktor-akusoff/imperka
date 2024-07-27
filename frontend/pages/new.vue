6f14698f3cd275a536e9d7a7318cb2c28f5a2a39
viktor-akusoff <kirill.voloshin.98@ya.ru>
2024-07-27 03:18:17 +0300

    restricting new page

--- frontend/pages/new.vue
+++ frontend/pages/new.vue
@@ -3,7 +3,29 @@
 </template>
 
 <script setup lang="ts">
+
+    import axios from 'axios'
+
+    const config = useRuntimeConfig()
+
     useHead({
         title: "НОВАЯ СТРАНИЦА"
+    })
+
+    const router = useRouter();
+
+    const authenticated = useState('authenticated')
+
+    onMounted(async () => {
+      const url = config.public.apiUrl + '/auth/check'
+      await axios
+        .get(url)
+        .then((response) => {
+            authenticated.value = true
+        })
+        .catch((error) => {
+            authenticated.value = false
+            router.push('/')
+        })   
     })
 </script>
