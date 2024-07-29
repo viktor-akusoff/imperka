5738a3a9dcc6c43b3b250ae298c8e2d3b3524b4a
viktor-akusoff <kirill.voloshin.98@ya.ru>
2024-07-29 04:15:38 +0300

    fixing entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@ export IMPERKA_SECRET_KEY=$(cat /opt/secret_key)
 export IMPERKA_PASSWORD=$(cat /opt/password)
 export IMPERKA_USERNAME=$(cat /opt/username)
 
-fastapi run app/main.py --port 8000 --reload --forwarded-allow-ips '*'
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload --forwarded-allow-ips '*'
